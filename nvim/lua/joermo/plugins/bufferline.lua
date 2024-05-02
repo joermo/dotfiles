9fc036cc569817009f7e6afcbf62c464dc3cef14
joermo <jomorgandev@gmail.com>
2024-05-02 17:05:40 -0400

    update bufferline and add fterm, oil, refactoring

--- nvim/lua/joermo/plugins/bufferline.lua
+++ nvim/lua/joermo/plugins/bufferline.lua
@@ -1,19 +1,19 @@
--- return {
---   "akinsho/bufferline.nvim",
---   options = {},
---   config = function()
---     local bufferline = require("bufferline")
---     bufferline.setup({
---       options = {
---         diagnostics = "nvim_lsp",
---         separator_style = "slant",
---         diagnostics_indicator = function(count, level)
---           local icon = level:match("error") and " " or " "
---           return " " .. icon .. count
---         end,
---       },
---     })
---   end,
--- }
-
-return {}
+return {
+  "akinsho/bufferline.nvim",
+  branch='main',
+  commit='f6f00d9ac1a51483ac78418f9e63126119a70709',
+  -- lazy=true,
+  config = function()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        diagnostics = "nvim_lsp",
+        separator_style = "slant",
+        diagnostics_indicator = function(count, level)
+          local icon = level:match("error") and " " or " "
+          return " " .. icon .. count
+        end,
+      },
+    })
+  end,
+}
