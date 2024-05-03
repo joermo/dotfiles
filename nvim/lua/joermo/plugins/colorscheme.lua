9c13852a14c895ceda94cc580737b798c11ca17a
joermo <jomorgandev@gmail.com>
2024-05-02 21:56:03 -0400

    update colorschemes

--- nvim/lua/joermo/plugins/colorscheme.lua
+++ nvim/lua/joermo/plugins/colorscheme.lua
@@ -1,9 +1,33 @@
+-- -- For Gruvbox
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--     require("gruvbox").setup({})
+--     vim.o.background = "dark"
+--     vim.cmd([[colorscheme gruvbox]])
+--   end,
+-- }
+
+-- -- For oxocarbon
+-- return {
+--   "B4mbus/oxocarbon-lua.nvim",
+--   priority = 1000,
+--   config = function()
+--     -- require("oxocarbon-lua").setup({})
+--     vim.o.background = "dark"
+--     vim.cmd([[colorscheme oxocarbon-lua]])
+--   end,
+-- }
+
+-- For Kanagawa
 return {
-  "ellisonleao/gruvbox.nvim",
+  "rebelot/kanagawa.nvim",
   priority = 1000,
   config = function()
-    require("gruvbox").setup({})
-    vim.o.background = "dark"
-    vim.cmd([[colorscheme gruvbox]])
+    require("kanagawa").setup({})
+    -- vim.o.background = "dark"
+    -- vim.cmd("colorscheme kanagawa-wave")
+    vim.cmd("colorscheme kanagawa-dragon")
   end,
 }
