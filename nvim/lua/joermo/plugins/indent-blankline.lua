14b71ce0675f636edd3f0fdebc30479462e7a01b
joermo <jomorgandev@gmail.com>
2024-05-28 01:17:03 -0400

    remove unused configs

--- nvim/lua/joermo/plugins/indent-blankline.lua
+++
@@ -1,2 +0,0 @@
--- return {}
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
--- nvim/lua/joermo/plugins/lualine.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = false,
-  config = function()
-    local lualine = require("lualine")
-    lualine.setup({})
-  end,
-}
