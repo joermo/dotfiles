f7b8426383b100605a47c69ad34ea5750076bcb4
joermo <jomorgandev@gmail.com>
2024-09-15 23:26:41 -0400

    add support for wsl

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -59,3 +59,18 @@ vim.opt.listchars = { tab = "▸ ", trail = "·" }
 vim.opt.timeoutlen = 1000
 
 vim.cmd("set notermsync") -- only works by calling vim builtin; fixes cursor teleporting/flickering with zellij
+
+if vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
