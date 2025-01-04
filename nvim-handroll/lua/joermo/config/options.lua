26fabdf1b99c6f604dcceafe9d0e0aae64dc4b9a
joermo <jomorgandev@gmail.com>
2025-01-04 11:04:27 -0500

    clean up options and telescope keybinds

--- nvim-handroll/lua/joermo/config/options.lua
+++ nvim-handroll/lua/joermo/config/options.lua
@@ -1,51 +1,44 @@
 local g = vim.g
-local o = vim.o
+local o = vim.opt
 
+-- Globals
+g.mapleader = " "
+g.maplocalleader = " "
+g.have_nerd_font = true
+g.noerrorbells = true
+g.noswapfile = true
+
+o.mouse = "a" -- enable mouse mode
+o.showmode = false
+vim.schedule(function() -- Schedule this  to not delay startup time
+  o.clipboard = "unnamedplus"
+end)
+o.breakindent = true
+o.undofile = true
+o.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+o.ignorecase = true
+o.smartcase = true
+o.signcolumn = "yes"
+o.updatetime = 250
+o.timeoutlen = 300
+o.splitright = true
+o.splitbelow = true
+o.list = true
+o.inccommand = "split"
+o.cursorline = true
+o.scrolloff = 10
+o.cmdheight = 0 -- set to 0 to use lualine
 o.tabstop = 2
 o.shiftwidth = 2
 o.softtabstop = 2
 o.expandtab = true
+o.guicursor = "" -- use block cursor
 o.number = true
 o.relativenumber = true
--- o.statuscolumn = "%s %l %r " -- Display absoluate and relative line numbers in status column
--- o.hidden = true
--- o.hidden = false -- todo re-enable this in case of any error
-o.ignorecase = true
-o.undodir = os.getenv("HOME") .. "/.nvim/undodir"
-o.undofile = true
-o.incsearch = true
-o.scrolloff = 10
-o.colorcolumn = "80"
-o.cmdheight = 0 -- set to 0 to use lualine
-o.updatetime = 50
 o.wrap = false
 o.linebreak = true -- wrap lines at work boundaries
-o.background = "dark"
-o.guicursor = ""
-o.errorbells = false
-o.smartindent = true
-o.smartcase = true
-o.signcolumn = "yes:1"
--- o.signcolumn = "yes"
-o.list = true
-o.clipboard = "unnamedplus"
-o.spell = false
-o.cursorline = true
-o.termguicolors = true
-o.foldlevel = 99
 
-g.signcolumn = true
-g.noerrorbells = true
-g.noswapfile = true
-g.nvim_tree_respect_buf_cwd = 1
-o.conceallevel = 0
-
--- test configs
-o.mouse = "a" -- Enable mouse mode
--- o.pumblend = 10 -- Popup blend
--- o.pumheight = 10 -- Maximum number of entries in a popup
-
-vim.opt.fillchars = {
+o.fillchars = {
   foldopen = "",
   foldclose = "",
   fold = " ",
@@ -53,7 +46,10 @@ vim.opt.fillchars = {
   diff = "╱",
   eob = " ",
 }
-vim.opt.listchars = { tab = "▸ ", trail = "·" }
-vim.opt.timeoutlen = 1000
+o.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 
+-- o.pumblend = 10 -- Popup blend
+-- o.pumheight = 10 -- Maximum number of entries in a popup
 -- vim.cmd("set notermsync") -- only works by calling vim builtin; fixes cursor teleporting/flickering with zellij
+-- o.signcolumn = "yes:1"
+-- o.conceallevel = 0
--- nvim-handroll/lua/joermo/plugins/telescope/telescope.lua
+++ nvim-handroll/lua/joermo/plugins/telescope/telescope.lua
@@ -1,86 +1,27 @@
-local bind = vim.keymap.set
-local custom_pickers = require("joermo.config.telescope.custom_pickers")
-
--- vim.ui.select = custom_pickers.select
-
--- ---------------------------
-
--- Fuzzy find files
-bind({ "n", "v" }, "'s", function()
-  custom_pickers.custom_workspace_grep("files", false, {})
-end, { desc = "Search Workspace Files" })
-bind({ "n", "v" }, "'S", function()
-  custom_pickers.custom_workspace_grep("files", true, {})
-end, { desc = "Search Workspace Files (Literal/No-grep)" })
-bind("n", "'b", function()
-  require("telescope.builtin").buffers({})
-end, { desc = "Search Currently Open Buffers" })
-
--- Fuzzy searching
-bind({ "n", "v" }, "'r", function()
-  custom_pickers.custom_workspace_grep("grep", false, {})
-end, { desc = "Live Grep Workspace" })
-bind({ "n", "v" }, "'R", function()
-  custom_pickers.custom_workspace_grep("grep", true, {})
-end, { desc = "Live Search Wrokspace (Literal/No-grep)" })
-bind("n", "'f", function()
-  require("telescope.builtin").live_grep({ grep_open_files = true })
-end, { desc = "Live Grep Open Files" })
-bind("n", "'c", function()
-  require("telescope.builtin").current_buffer_fuzzy_find({})
-end, { desc = "Live Grep Current File" })
-
--- LSP Binds
-bind("n", "<leader>O", function()
-  require("telescope.builtin").lsp_workspace_symbols({})
-end, { desc = "Show Workspace Symbols" })
-bind("n", "<leader>o", function()
-  local ft = vim.bo.filetype
-  if ft ~= "yaml" then
-    require("telescope.builtin").lsp_document_symbols({})
-  else
-    custom_pickers.yaml_symbols({})
-  end
-end, { desc = "Show Document Symbols" })
-bind("n", "gd", function()
-  require("telescope.builtin").lsp_definitions({ reuse_win = true })
-end)
-bind("n", "gi", function()
-  require("telescope.builtin").lsp_implementations({ reuse_win = true })
-end)
-bind("n", "gr", function()
-  require("telescope.builtin").lsp_references({ reuse_win = true })
-end, { desc = "References" })
-bind("n", "gt", function()
-  require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
-end)
--- Git
-bind("n", "'gs", function()
-  require("telescope.builtin").git_status({})
-end, { desc = "Show Git Status" })
-
--- Misc
-bind("n", "'km", function()
-  require("telescope.builtin").keymaps({})
-end, { desc = "Select From Telescope Builtins" })
-bind("n", "'ts", function()
-  require("telescope.builtin").builtin({})
-end, { desc = "Select From Telescope Builtins" })
-
-----------------------------------------------------------------------
-
 return {
   "nvim-telescope/telescope.nvim",
-  keys = {
-    -- disable the keymap to grep files
-    { "<leader>/", false },
+  event = "VimEnter",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      cond = function()
+        return vim.fn.executable("make") == 1
+      end,
+    },
+    { "nvim-telescope/telescope-ui-select.nvim" },
+    { "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
   },
-  opts = function()
+  config = function()
+    local telescope = require("telescope")
     local actions = require("telescope.actions")
-    return {
+    telescope.setup({
       defaults = {
         mappings = {
           i = {
+            ["<c-enter>"] = actions.to_fuzzy_refine,
             ["<ESC>"] = actions.close,
             ["<C-j>"] = actions.move_selection_next,
             ["<C-k>"] = actions.move_selection_previous,
@@ -94,14 +35,7 @@ return {
           },
         },
       },
-      layout_config = {
-        -- vertical = { width = 0.5 },
-      },
-      pickers = {
-        find_files = {
-          -- theme = "dropdown",
-        },
-      },
+      pickers = {},
       file_ignore_patterns = {
         "./node_modules/*",
         "node_modules",
@@ -109,6 +43,82 @@ return {
         "node_modules/*",
         "*venv*",
       },
-    }
+      extensions = {
+        ["ui-select"] = {
+          require("telescope.themes").get_dropdown(),
+        },
+      },
+    })
+    pcall(require("telescope").load_extension, "fzf")
+    pcall(require("telescope").load_extension, "ui-select")
+    local builtin = require("telescope.builtin")
+    local bind = vim.keymap.set
+    local custom_pickers = require("joermo.config.telescope.custom_pickers")
+    -- Fuzzy find files
+    bind({ "n", "v" }, "'s", function()
+      custom_pickers.custom_workspace_grep("files", false, {})
+    end, { desc = "Search Workspace Files" })
+    bind({ "n", "v" }, "'S", function()
+      custom_pickers.custom_workspace_grep("files", true, {})
+    end, { desc = "Search Workspace Files (Literal/No-grep)" })
+    bind("n", "'b", function()
+      require("telescope.builtin").buffers({})
+    end, { desc = "Search Currently Open Buffers" })
+
+    -- Fuzzy searching
+    bind({ "n", "v" }, "'r", function()
+      custom_pickers.custom_workspace_grep("grep", false, {})
+    end, { desc = "Live Grep Workspace" })
+    bind({ "n", "v" }, "'R", function()
+      custom_pickers.custom_workspace_grep("grep", true, {})
+    end, { desc = "Live Search Wrokspace (Literal/No-grep)" })
+    bind("n", "'f", function()
+      require("telescope.builtin").live_grep({ grep_open_files = true })
+    end, { desc = "Live Grep Open Files" })
+    bind("n", "'c", function()
+      require("telescope.builtin").current_buffer_fuzzy_find({})
+    end, { desc = "Live Grep Current File" })
+
+    -- LSP Binds
+    bind("n", "<leader>O", function()
+      require("telescope.builtin").lsp_workspace_symbols({})
+    end, { desc = "Show Workspace Symbols" })
+    bind("n", "<leader>o", function()
+      local ft = vim.bo.filetype
+      if ft ~= "yaml" then
+        require("telescope.builtin").lsp_document_symbols({})
+      else
+        custom_pickers.yaml_symbols({})
+      end
+    end, { desc = "Show Document Symbols" })
+    bind("n", "gd", function()
+      require("telescope.builtin").lsp_definitions({ reuse_win = true })
+    end)
+    bind("n", "gi", function()
+      require("telescope.builtin").lsp_implementations({ reuse_win = true })
+    end)
+    bind("n", "gr", function()
+      require("telescope.builtin").lsp_references({ reuse_win = true })
+    end, { desc = "References" })
+    bind("n", "gt", function()
+      require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+    end)
+
+    -- Git
+    bind("n", "'gs", function()
+      require("telescope.builtin").git_status({})
+    end, { desc = "Show Git Status" })
+
+    -- Misc
+    bind("n", "'km", function()
+      require("telescope.builtin").keymaps({})
+    end, { desc = "Select From Telescope Builtins" })
+    bind("n", "'ts", function()
+      require("telescope.builtin").builtin({})
+    end, { desc = "Select From Telescope Builtins" })
+
+    bind("n", "<leader>sn", function()
+      builtin.find_files({ cwd = vim.fn.stdpath("config") })
+    end, { desc = "[S]earch [N]eovim files" })
   end,
 }
