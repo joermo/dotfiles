38d1d559624a092856944c60a763480ced4519df
joermo <jomorgandev@gmail.com>
2025-09-07 07:43:19 -0400

    add ai plugins

---
+++ nvim-handroll/lua/joermo/plugins/ai/copilot-chat.lua
@@ -0,0 +1,22 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", branch = "master" }
+  },
+  build = "make tiktoken",
+  config = function ()
+    local opts = {}
+    local copilot_chat = require("CopilotChat")
+    copilot_chat.setup(opts)
+    local bind = vim.keymap.set
+    bind("n", "<leader>cc", function()
+      copilot_chat.toggle()
+    end, { desc = "Toggle GH Copilot Chat" })
+    bind("n", "<leader>cr", function()
+      copilot_chat.reset()
+    end, { desc = "Reset GH Copilot Chat" })
+    bind("n", "<leader>ca", function()
+      copilot_chat.close()
+    end, { desc = "Close GH Copilot Chat" })
+  end
+}
--- nvim-handroll/lua/joermo/plugins/ai/copilot.lua
+++ nvim-handroll/lua/joermo/plugins/ai/copilot.lua
@@ -3,21 +3,29 @@ return {
   cmd = "Copilot",
   build = ":Copilot auth",
   event = "BufReadPost",
-  opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
+  config = function()
+    local copilot = require("copilot")
+    local opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = false,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
       },
-    },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-      help = true,
-    },
-  },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      }
+    }
+    copilot.setup(opts)
+    local bind = vim.keymap.set
+    bind("n", "<leader>cp", function()
+      require("copilot.panel").toggle()
+    end, { desc = "Toggle GH Copilot Suggestion Panel" })
+  end
 }
