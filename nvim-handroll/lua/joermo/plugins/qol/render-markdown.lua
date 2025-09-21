3967417b1dac7e3b7db165572e2e101981e089c2
joermo <jomorgandev@gmail.com>
2025-09-20 20:25:32 -0400

    disable render-markdown

--- nvim-handroll/lua/joermo/plugins/qol/render-markdown.lua
+++ nvim-handroll/lua/joermo/plugins/qol/render-markdown.lua
@@ -1,23 +1,24 @@
-return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    pipe_table = {
-      -- style = "normal",
-      cell = "trimmed",
-    },
-    code = {
-      sign = false,
-      width = "block",
-      right_pad = 1,
-    },
-    heading = {
-      sign = false,
-      icons = {},
-    },
-  },
-}
+return {}
+-- return {
+--   "MeanderingProgrammer/render-markdown.nvim",
+--   dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+--   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+--   -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+--   ---@module 'render-markdown'
+--   ---@type render.md.UserConfig
+--   opts = {
+--     pipe_table = {
+--       -- style = "normal",
+--       cell = "trimmed",
+--     },
+--     code = {
+--       sign = false,
+--       width = "block",
+--       right_pad = 1,
+--     },
+--     heading = {
+--       sign = false,
+--       icons = {},
+--     },
+--   },
+-- }
