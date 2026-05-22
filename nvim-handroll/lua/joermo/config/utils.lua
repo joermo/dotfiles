74818ce687671c0dca7e829e2a5bfe0dbfb423ed
Joseph Morgan <joermo@Josephs-MacBook-Pro.local>
2026-05-22 19:02:35 -0400

    relocate formatting utils into formatting config

--- nvim-handroll/lua/joermo/config/utils.lua
+++ nvim-handroll/lua/joermo/config/utils.lua
@@ -41,35 +41,6 @@ M.copyFilePathAndLineNumber = function()
   end
 end
 
--- Custom function to invoke conform formatting
-M.conform_format = function()
-  local conform = require("conform")
-  conform.format({
-    lsp_fallback = true,
-    async = false,
-    timeout_ms = 10000,
-  })
-end
-
--- Custom function to format by range via objects and motions
-M.format_range_operator = function()
-  local conform = require("conform")
-  local old_func = vim.go.operatorfunc
-  _G.op_func_formatting = function()
-    local opts = {
-      range = {
-        ["start"] = vim.api.nvim_buf_get_mark(0, "["),
-        ["end"] = vim.api.nvim_buf_get_mark(0, "]"),
-      },
-    }
-    conform.format(opts)
-    vim.go.operatorfunc = old_func
-    _G.op_func_formatting = nil
-  end
-  vim.go.operatorfunc = "v:lua.op_func_formatting"
-  vim.api.nvim_feedkeys("g@", "n", false)
-end
-
 M.close_all_buffers_but_current_and_provided = function(keep_types)
   local buffers = vim.api.nvim_list_bufs()
   local current_buf = vim.api.nvim_get_current_buf()
