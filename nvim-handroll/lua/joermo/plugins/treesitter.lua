05ddf1a788a1036224b75f4915b28bd364ce5600
Joseph Morgan <joermo@Josephs-MacBook-Pro.local>
2026-05-22 22:53:33 -0400

    update treesitter configs

--- nvim-handroll/lua/joermo/plugins/treesitter.lua
+++ nvim-handroll/lua/joermo/plugins/treesitter.lua
@@ -1,67 +1,59 @@
-local ts_ensure_installed = {
-  "bash",
-  "c",
-  "diff",
-  "html",
-  "javascript",
-  "jsdoc",
-  "json",
-  "jsonc",
-  "lua",
-  "luadoc",
-  "luap",
-  "markdown",
-  "markdown_inline",
-  "query",
-  "regex",
-  "toml",
-  "tsx",
-  "typescript",
-  "vim",
-  "vimdoc",
-  "xml",
-  "yaml",
-  "go",
-  -- Python:
-  "python",
-  "ninja",
-  "rst",
-  -- Rust
-  "rust",
-  "ron",
-  "terraform",
-}
-
 return {
   "nvim-treesitter/nvim-treesitter",
-  version = false, -- last release is way too old and doesn't work on Windows
+  branch = "main",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufWritePost", "BufNewFile", "VeryLazy" },
-  init = function(plugin)
-    -- require("lazy.core.loader").add_to_rtp(plugin)
-    require("nvim-treesitter.query_predicates")
-  end,
   dependencies = {},
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
   keys = {
     { "<c-space>", desc = "Increment Selection" },
-    { "<bs>", desc = "Decrement Selection", mode = "x" },
+    { "<bs>",      desc = "Decrement Selection", mode = "x" },
   },
-  opts = {
-    highlight = { enable = true },
-    indent = { enable = true },
-    ensure_installed = ts_ensure_installed,
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = false,
-        node_decremental = "<bs>",
+  config = function()
+    local DISABLE_HIGHLIGHT_THRESHOLD = 30000
+    require("nvim-treesitter").setup({
+      ensure_installed = {
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+        "go",
+        -- Python:
+        "python",
+        "ninja",
+        "rst",
+        -- Rust
+        "rust",
+        "ron",
+        "terraform",
       },
-    },
-  },
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+      auto_install = true,
+      sync_install = false,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+        disable = function(lang, bufnr)
+          return vim.api.nvim_buf_line_count(bufnr or 0) > DISABLE_HIGHLIGHT_THRESHOLD
+        end,
+      }
+    })
+  end
 }
