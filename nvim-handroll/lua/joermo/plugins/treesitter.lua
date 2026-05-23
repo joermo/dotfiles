b1d772398d900f07dc4bdb008e2b0448f87b15d3
joermo <jomorgandev@gmail.com>
2026-05-23 16:33:53 -0400

    add treesitter-textobjects

--- nvim-handroll/lua/joermo/plugins/treesitter.lua
+++ nvim-handroll/lua/joermo/plugins/treesitter.lua
@@ -1,42 +1,86 @@
 return {
-  -- Requires tree-sitter-cli installed
-  -- (brew install tree-sitter-cli)
-  "arborist-ts/arborist.nvim",
-  lazy = false,
-  config = function()
-    require("arborist").setup({
-      ensure_installed = {
-        "bash",
-        "c",
-        "diff",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        --"jsonc",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "query",
-        "regex",
-        "toml",
-        "tsx",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-        "go",
-        "python",
-        "ninja",
-        "rst",
-        "rust",
-        "ron",
-        --"terraform",
-      },
-      auto_install = true,
-    })
-  end,
+  {
+    -- Requires tree-sitter-cli installed
+    -- (brew install tree-sitter-cli)
+    "arborist-ts/arborist.nvim",
+    lazy = false,
+    config = function()
+      require("arborist").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "diff",
+          "html",
+          "javascript",
+          "jsdoc",
+          "json",
+          --"jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "query",
+          "regex",
+          "toml",
+          "tsx",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "xml",
+          "yaml",
+          "go",
+          "python",
+          "ninja",
+          "rst",
+          "rust",
+          "ron",
+          --"terraform",
+        },
+        auto_install = true,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            -- ['@class.outer'] = '<c-v>', -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+      })
+      local ts_select = require("nvim-treesitter-textobjects.select")
+      local bind = vim.keymap.set
+      -- keymaps
+      -- You can use the capture groups defined in `textobjects.scm`
+      bind({ "x", "o" }, "af", function()
+        ts_select.select_textobject("@function.outer", "textobjects")
+      end)
+      bind({ "x", "o" }, "if", function()
+        ts_select.select_textobject("@function.inner", "textobjects")
+      end)
+      bind({ "x", "o" }, "ac", function()
+        ts_select.select_textobject("@class.outer", "textobjects")
+      end)
+      bind({ "x", "o" }, "ic", function()
+        ts_select.select_textobject("@class.inner", "textobjects")
+      end)
+      -- You can also use captures from other query groups like `locals.scm`
+      bind({ "x", "o" }, "as", function()
+        ts_select.select_textobject("@local.scope", "locals")
+      end)
+    end,
+  },
 }
